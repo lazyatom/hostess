ebc438342250d8b281ca495255292b7b953011a7
Chris Roos <chris@seagul.co.uk>
2010-11-25 16:14:05 +0000

    This is petty but I'd prefer not to have spaces at the beginning of the virtual host config files that hostess generates.

--- lib/hostess/virtual_host.rb
+++ lib/hostess/virtual_host.rb
@@ -47,25 +47,25 @@ module Hostess
       def vhost_config
         domain, directory = @options.domain, @options.directory
         template = <<-EOT
-        <VirtualHost *:80>
-          ServerName <%= domain %>
-          DocumentRoot <%= directory %>
-          <Directory <%= directory %>>
-            Options FollowSymLinks
-            AllowOverride All
-            allow from all
-          </Directory>
-          <DirectoryMatch "^/.*/\.svn/">
-            ErrorDocument 403 /404.html
-            Order allow,deny
-            Deny from all
-            Satisfy All
-          </DirectoryMatch>
-          ErrorLog <%= File.join(apache_log_directory, 'error_log') %>
-          CustomLog <%= File.join(apache_log_directory, 'access_log') %> common
-          #RewriteLogLevel 3
-          RewriteLog <%= File.join(apache_log_directory, 'rewrite_log') %>
-        </VirtualHost>
+<VirtualHost *:80>
+  ServerName <%= domain %>
+  DocumentRoot <%= directory %>
+  <Directory <%= directory %>>
+    Options FollowSymLinks
+    AllowOverride All
+    allow from all
+  </Directory>
+  <DirectoryMatch "^/.*/\.svn/">
+    ErrorDocument 403 /404.html
+    Order allow,deny
+    Deny from all
+    Satisfy All
+  </DirectoryMatch>
+  ErrorLog <%= File.join(apache_log_directory, 'error_log') %>
+  CustomLog <%= File.join(apache_log_directory, 'access_log') %> common
+  #RewriteLogLevel 3
+  RewriteLog <%= File.join(apache_log_directory, 'rewrite_log') %>
+</VirtualHost>
         EOT
         ERB.new(template).result(binding)
       end
